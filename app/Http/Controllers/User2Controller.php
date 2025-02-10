b30be59cbb8c88296e23d2cd9a3ecff84d9df0cf
Dabananda Mitra <imdmitra@gmail.com>
2025-02-10 15:09:13 +0700

    user2

---
+++ app/Http/Controllers/User2Controller.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User2;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User2::all();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Users retrieved succesfully',
+            'data' => $users
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string',
+            'password' => 'required|string',
+            'username' => 'required|string',
+            'phone' => 'required|integer'
+        ]);
+
+        $users = User2::create($request->all());
+
+        return response()->json([
+            'status' => 201,
+            'message' => 'User created succesfully',
+            'data' => $users
+        ], 201);
+    }
+
+    public function show($id)
+    {
+        $users = User2::find($id);
+
+        if (!$users) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'User not found',
+                'data' => null
+            ],404);
+        }
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'User retrieved succesfully',
+            'data' => $users
+        ], 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $users = User2::find($id);
+
+        if(!$users) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'User not found',
+                'data' => null
+            ], 404);
+        }
+
+        $request->validate([
+            'name' => 'string|max:255',
+            'email' => 'required|string',
+            'password' => 'required|string',
+            'username' => 'required|string',
+            'phone' => 'required|integer'
+        ]);
+        $users->update($request->all());
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'User updated succesfully',
+            'data' => $users
+        ], 200);
+    }
+
+    public function destroy($id)
+    {
+        $users = User2::find($id);
+
+        if(!$users) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'User not found',
+                'data' => null
+            ], 404);
+        }
+
+        $users->delete();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'User deleted succesfully',
+            'data' => null
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User2Controller;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,8 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/categories', [CategoryController::class,'index']);
 Route::apiResource('/store', CategoryController::class);
+
+Route::post('/user2', [User2Controller::class, 'store']);
+Route::get('/user2/{id}', [User2Controller::class, 'show']);
+Route::delete('/user2/{id}', [User2Controller::class, 'destroy']);
+Route::put('/user2/{id}', [User2Controller::class, 'update']);
